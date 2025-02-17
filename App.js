6cc4c1e33b7367dae212bb1803499b3dd92c8751
chennupati109 <vasuchennupati20@gmail.com>
2025-02-17 10:17:32 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -68,59 +68,75 @@
 // }
 // export default App;
 
-import React, { useReducer } from 'react';
+// import React, { useReducer } from 'react';
 
-function appleReducer(state = 3, action) {
-  switch (action.type) {
-    case 'ADD_APPLE':
-      return state + 1;
-    case 'REMOVE_APPLE':
-      return state - 1;
-    default:
-      return state;
+// function appleReducer(state = 3, action) {
+//   switch (action.type) {
+//     case 'ADD_APPLE':
+//       return state + 1;
+//     case 'REMOVE_APPLE':
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// }
+
+// function App() {
+//   const [state, dispatch] = useReducer(appleReducer, 3);
+
+//   return (
+//     <div style={{ padding: '20px', textAlign: 'center' }}>
+//       <h1 style={{ color: 'orange' }}>APPLE Counter</h1>
+//       <p style={{color: 'bule' }}> Number of APPLES: {state}</p>
+//       <button
+//         onClick={() => dispatch({ type: 'ADD_APPLE' })}
+//         style={{
+//           backgroundColor: 'blue',
+//           color: 'green',
+//           padding: '10px 20px',
+//           margin: '10px',
+//           border: 'none',
+//           borderRadius: '20px',
+//           cursor: 'pointer',
+//         }}
+//       >
+//         ADD APPLE
+//       </button>
+//       <button
+//         onClick={() => dispatch({ type: 'REMOVE_APPLE' })}
+//         style={{
+//           backgroundColor: 'yellow',
+//           color: 'pink',
+//           padding: '10px 20px',
+//           margin: '10px',
+//           border: 'none',
+//           borderRadius: '20px',
+//           cursor: 'pointer',
+//         }}
+//       >
+//         REMOVE APPLE
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React,{useState} from "react";
+
+function App(){
+  const[text,setText] = useState("")
+  
+  const handleChange = (event) => {
+    setText(event.target.value)
   }
-}
 
-function App() {
-  const [state, dispatch] = useReducer(appleReducer, 3);
-
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1 style={{ color: 'orange' }}>APPLE Counter</h1>
-      <p style={{color: 'bule' }}> Number of APPLES: {state}</p>
-      <button
-        onClick={() => dispatch({ type: 'ADD_APPLE' })}
-        style={{
-          backgroundColor: 'blue',
-          color: 'green',
-          padding: '10px 20px',
-          margin: '10px',
-          border: 'none',
-          borderRadius: '20px',
-          cursor: 'pointer',
-        }}
-      >
-        ADD APPLE
-      </button>
-      <button
-        onClick={() => dispatch({ type: 'REMOVE_APPLE' })}
-        style={{
-          backgroundColor: 'yellow',
-          color: 'pink',
-          padding: '10px 20px',
-          margin: '10px',
-          border: 'none',
-          borderRadius: '20px',
-          cursor: 'pointer',
-        }}
-      >
-        REMOVE APPLE
-      </button>
+  return(
+    <div>
+      <input type="text" value={text} onChange={handleChange}/>
+      <p>Your Text:{text}</p>
     </div>
-  );
+    
+  )
 }
-
 export default App;
-
-
-
